ace3e59bba3660f7aa4296c1d87c4eba14cf7567
hasindu methmal <methghk@gmail.com>
2025-05-30 23:11:26 +0530

    change 1.0

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,10 +25,10 @@ class Kernel extends ConsoleKernel
      * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
      * @return void
      */
-    protected function schedule(Schedule $schedule)
-    {
-        $schedule->command('reset:update-count')->daily();
-    }
+        protected function schedule(Schedule $schedule)
+        {
+            $schedule->command('reset:update-count')->daily();
+        }
 
     /**
      * The commands to be registered.
