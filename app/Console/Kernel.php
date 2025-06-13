2d5bc2a3ca46d793d01eedf3c39143f5a116f6ae
HasinduMethmal <methghk@gmail.com>
2025-06-13 16:28:30 +0530

    kernel_bug

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,7 +27,7 @@ class Kernel extends ConsoleKernel
      */
         protected function schedule(Schedule $schedule)
         {
-            $schedule->command('app:post-history-update')->dailyAt('23:59');
+            $schedule->command('app:post-history-update')->dailyAt('00:00');
             $schedule->command('reset:update-count')->dailyAt('00:00');
             $schedule->command('conversion:fetch-rate')->dailyAt('00:00');
         }
