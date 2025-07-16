1bd2137d6d28ae73f57bf3225fe588dd0b51d4e0
GTec <gayashancs7@gmail.com>
2025-07-16 10:03:55 +0530

    Fix cv3 issues

--- resources/views/User/cv3.blade.php
+++ resources/views/User/cv3.blade.php
@@ -687,7 +687,7 @@ $sidebarColor = request()->query('sidebar_color', $user->sidebar_color ?? '#0A4D
                 sidebar.classList.add('tall2');
                 sidebar.classList.remove('tall');
                 sidebar.classList.remove('tall3');
-            } else if(totalHeight > 3300 && totalHeight < 4488) {
+            } else if(totalHeight > 3200 && totalHeight < 4488) {
                 sidebar.classList.remove('tall2');
                 sidebar.classList.remove('tall');
                 sidebar.classList.add('tall3');
